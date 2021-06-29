191517258f57f386075bf615189a9d70ef114980
Suresh Raju Bethalam <sureshraju.bethalam@carrier.com>
2021-06-29 15:10:58 +0530

    asw sample demo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace awssample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Suresh!");
+        }
+    }
+}
